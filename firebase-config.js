711d9af19fae3720546386bdd570cf86375d0f79
bvlencilenci <nyvnpvsu7thekvmi.300@gmail.com>
2025-03-12 19:08:14 +0000

    test roster pics

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCt3InzocitfJcKken1EYLJbihwBECtYcI",
+  authDomain: "immortal-raindops-databa.firebaseapp.com",
+  projectId: "immortal-raindops-databa",
+  storageBucket: "immortal-raindops-databa.firebasestorage.app",
+  messagingSenderId: "1098265239221",
+  appId: "1:1098265239221:web:a039a4802a6d6133df28fc",
+  measurementId: "G-RQGZEWFS6F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
