c2218df04c50f099df33000448930e4e6f00cb6b
bvlencilenci <nyvnpvsu7thekvmi.300@gmail.com>
2026-01-11 00:44:50 +0000

    chore: update task list and trigger redeploy

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -5,7 +5,7 @@ export default async function Archive() {
   const tracks = await getTracks();
 
   return (
-    <main className="flex-1 w-full flex flex-col bg-black px-8 pt-24 lg:pt-0">
+    <main className="flex-1 w-full flex flex-col bg-black px-8 pt-[90px]">
       <ArchiveGrid tracks={tracks} />
     </main>
   );
